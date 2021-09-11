9e25141fe341f8fc3aaed600ecd29a1e3a1473e8
petk1234 <petkun8034@gmail.com>
2021-09-11 20:42:58 +0300

    Final commit/version

--- src/index.js
+++ src/index.js
@@ -1,66 +1,87 @@
+import * as basicLightbox from 'basiclightbox';
+import "basiclightbox/dist/basicLightbox.min.css";
 import imagesPromise from "./apiService.js";
 import template from "./template.hbs";
+import "./styles.css";
+import buttonEl from "./button-creation";
 const refs = {
     bodyEl: document.querySelector('body'),
     inputEl: document.querySelector('input'),
+    ulEl: document.querySelector('ul'),
+    //photoCardEl: document.querySelector('.photo-card'),
 }
 let page = 1;
+// refs.bodyEl.appendChild(buttonEl);
 
+const deleteUl = () =>{
+    const divEl = document.querySelectorAll('div');
+    divEl.forEach( div => div.remove() ); 
+}
 
 const insertImages = (e) =>{
 
-    const ulEl = document.querySelectorAll('ul');
-    ulEl.forEach( ul => ul.remove() );
+    deleteUl();
 
     if(e.target.value !== ''){
-    console.log(imagesPromise(e.target.value, page));
-    imagesPromise(e.target.value, page)
-    .then( datas =>{ 
-       return datas.hits.map( data =>{
-           //console.log(data.pageURL);
-           //console.log(data);
-           refs.bodyEl.insertAdjacentHTML('beforeend', template(data));
-           //return data.pageURL;
-       });
-    })
-    .then( smth =>{
-        page = page + 1;
-        console.log(page);
-        return page;
-    });
-    // page += 1;
-    // console.log(page);
-    }
-    else{
-        const ulEll = document.querySelectorAll('ul');
-        ulEll.forEach( ul => ul.remove() );
+      console.log(imagesPromise(e.target.value, page));
+      imagesPromise(e.target.value, page, 12)
+      .then( datas =>{ 
+          if(e.target.value !== ''){   
+            datas.hits.map( data =>{
+                //console.log(data.pageURL);
+                //console.log(data);
+                refs.ulEl.insertAdjacentHTML('afterbegin', template(data));
+                //return data.pageURL;
+            });
+            //page = page + 1;
+            console.log(page);
+          }
 
-        page = 1;
-        console.log(page);
-         imagesPromise(e.target.value, page)
-         .then( datas =>{ 
-          //return datas.hits.map( data =>{
-            //console.log(data.pageURL);
-            //console.log(data);
-    //        refs.bodyEl.insertAdjacentHTML('beforeend', template(data));
-            //return data.pageURL;
-          const ulEll = document.querySelectorAll('ul');
-          ulEll.forEach( ul => ul.remove() );
-          page = 1;
-          console.log(page);
-          return page;
-        });
-    // });
+          else{
+               deleteUl();
+               page = 1;
+               console.log('promise else: ', page);
+          }
+      })
+    }
+     
+    else{
+         const ulEll = document.querySelectorAll('ul');
+         ulEll.forEach( ul => ul.remove() );
+         page = 1;
+         console.log('outside else: ', page);
     }
 }
+let n = 0;
+const scrollingNextImgs = e =>{
+    //console.log(refs.inputEl.value);
+    console.log(imagesPromise(refs.inputEl.value, page, 6));
+    imagesPromise(refs.inputEl.value, page, 6)
+      .then( datas =>{ 
+            datas.hits.map( data =>{
+                refs.ulEl.insertAdjacentHTML('beforeend', template(data));
+            });
+            
+            window.scrollBy({ 
+                top: window.innerHeight / 1.5,
+                behavior: "smooth",
+            });
+            page = page + 1;
+            console.log(page);
+            n = n + 1;
+      });
+}
 
+const openImgLightbox = e =>{
+    const instance = basicLightbox.create(`
+    <img src=${e.target.src} width="800" height="600">`,{
+        closable: true,
+    });
+    instance.show();
+    console.log('hello');
+}
 
-// insertImages('flowers', 1);
-// const inputText = e =>{
-//     console.log(e.target.value);
-//     return e.target.value;
-// }
-//refs.inputEl.addEventListener('input', inputText);
 refs.inputEl.addEventListener('input', insertImages);
-//refs.bodyEl.insertAdjacentHTML('afterbegin', insertImages('flowers', 1));
-console.log("hi");
+refs.bodyEl.append(buttonEl);
+buttonEl.addEventListener('click', scrollingNextImgs);
+refs.ulEl.addEventListener('click', openImgLightbox);
